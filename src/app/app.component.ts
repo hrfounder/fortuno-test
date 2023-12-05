f46c15464825141add500211e3149d347d1bd44e
Ivan Komljenovic <hrfounder@gmail.com>
2023-12-05 17:16:02 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit,NgZone } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SocialAuthService, GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  standalone:true,
+  imports:[RouterOutlet,HttpClientModule,CommonModule,GoogleSigninButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fortuno-test';
+export class AppComponent implements OnInit {
+  data: any[];
+  user:any
+
+  constructor(private http: HttpClient, private authService:SocialAuthService) {
+    this.data=[]
+    this.user=null
+  }
+
+  ngOnInit() {
+    this.loadData();
+    this.authService.authState.subscribe((user) => {
+      this.user = user
+      //perform further logics
+    });
+
+  }
+
+  loadData() {
+    this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+      this.data = data;
+    });
+  }
+  signOut(){
+    this.authService.signOut()
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import {
+  GoogleLoginProvider,
+} from '@abacritt/angularx-social-login';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '175788529847-1qi3ikhpviodtoua65c7a637vaifevus.apps.googleusercontent.com'
+          )
+        }
+      ],
+      onError: (error) => {
+        console.error(error);
+      }
+    } as SocialAuthServiceConfig},provideRouter(routes), provideClientHydration()]
+
 };
